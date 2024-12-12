3d9eb29b77625aa5cce0d8d838a7609cd72ce751
Kris1027 <obarzanek.work@gmail.com>
2024-12-12 12:51:25 +0100

    create validateTask validator

---
+++ src/validators/task-validator.ts
@@ -0,0 +1,14 @@
+import z from 'zod';
+
+export const validateTask = z.object({
+    title: z
+        .string()
+        .trim()
+        .min(3, 'Title must be at least 3 characters long')
+        .max(30, 'Title can be a maximum 30 characters long'),
+    text: z
+        .string()
+        .trim()
+        .min(6, 'Text must be at least 6 characters long')
+        .max(500, 'Text can be a maximum 500 characters long'),
+});
