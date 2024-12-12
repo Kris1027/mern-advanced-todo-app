eccd7d89fbedcc01f114fd7fc9bb661607ad5a13
Kris1027 <obarzanek.work@gmail.com>
2024-12-12 16:10:08 +0100

    create updateTask controller

--- src/controllers/task-controller.ts
+++ src/controllers/task-controller.ts
@@ -15,7 +15,7 @@ export const createTask = async (req: Request, res: Response, next: NextFunction
 
         if (!req.user || !req.user._id) {
             const error: ErrorProps = new Error('User not logged in');
-            error.status = 404;
+            error.status = 401;
             return next(error);
         }
 
@@ -97,6 +97,58 @@ export const deleteTask = async (req: Request, res: Response, next: NextFunction
 
         await Task.findByIdAndDelete(req.params.id);
         res.status(200).json({ message: 'Task deleted successfully', task });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateTask = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { title, text } = req.body;
+        if (!title || !text) {
+            const error: ErrorProps = new Error('Inputs cannot be empty');
+            error.status = 400;
+            return next(error);
+        }
+
+        if (!req.user || !req.user._id) {
+            const error: ErrorProps = new Error('User not logged in');
+            error.status = 401;
+            return next(error);
+        }
+
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user) {
+            const error: ErrorProps = new Error('User not found');
+            error.status = 404;
+            return next(error);
+        }
+        const taskId = req.params.id;
+        let task = await Task.findById(taskId);
+        if (!task) {
+            const error: ErrorProps = new Error('Task not found');
+            error.status = 404;
+            return next(error);
+        }
+
+        if (task.user.toString() !== userId.toString()) {
+            const error: ErrorProps = new Error('Not authorized to update this task');
+            error.status = 401;
+            return next(error);
+        }
+
+        const update = {
+            title,
+            text,
+        };
+
+        task = await Task.findByIdAndUpdate(taskId, update);
+        const updatedTask = await Task.findById(taskId);
+        res.status(200).json({
+            message: `Task of ${user.username} updated successfully`,
+            updatedTask,
+        });
     } catch (error) {
         next(error);
     }
--- src/routes/task-route.ts
+++ src/routes/task-route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTask, deleteTask, getUserTasks } from '../controllers/task-controller';
+import { createTask, deleteTask, getUserTasks, updateTask } from '../controllers/task-controller';
 import { protectRoute } from '../utils/protect-route';
 import { validateSchema } from '../utils/validate-schema';
 import { validateTask } from '../validators/task-validator';
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/', protectRoute, validateSchema(validateTask), createTask);
 router.get('/', protectRoute, getUserTasks);
 router.delete('/:id', protectRoute, deleteTask);
+router.put('/:id', protectRoute, validateSchema(validateTask), updateTask);
 
 export default router;
