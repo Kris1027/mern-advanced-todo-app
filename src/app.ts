324efe5df8fba3b99b3a13965ed381a6b8ebb52e
Kris1027 <obarzanek.work@gmail.com>
2024-12-13 07:55:42 +0100

    preparing to deploy

--- src/app.ts
+++ src/app.ts
@@ -27,14 +27,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/dist'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    });
-}
-
 app.use('/api/auth', authRouter);
 app.use('/api/tasks', taskRouter);
 
