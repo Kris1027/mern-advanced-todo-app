fb5b7691c57870f34ba678ba414f7fae7879ae00
Kris1027 <obarzanek.work@gmail.com>
2024-12-16 07:13:04 +0100

    refactor login page, create schemas folder

---
+++ client/src/pages/login-page.tsx
@@ -0,0 +1,98 @@
+import { Link, useNavigate } from '@tanstack/react-router';
+import { useMutation } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+import { LogIn } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema } from '@/schemas/login-schema';
+import { useToast } from '@/hooks/use-toast';
+import LoadingSpinner from '@/components/loading-spinner';
+import Logo from '@/components/logo';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+const LoginPage = () => {
+    const navigate = useNavigate();
+    const { toast } = useToast();
+
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+        reset,
+    } = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const { mutate, isPending } = useMutation({
+        mutationFn: async (values: z.infer<typeof loginSchema>) => {
+            const res = await axios.post('/api/auth/login', values);
+            return res.data;
+        },
+        onSuccess: (data) => {
+            toast({ title: data.message, variant: 'positive' });
+            navigate({ to: '/' });
+            reset();
+        },
+        onError: (error: AxiosError<{ message: string }>) => {
+            toast({
+                title:
+                    error.response?.data?.message ||
+                    error.message ||
+                    'An unexpected error occurred',
+                variant: 'destructive',
+            });
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof loginSchema>) {
+        mutate(values);
+    }
+    return (
+        <main className='w-full flex gap-10 min-h-screen justify-center items-center'>
+            <Logo size='md' />
+            <div className='w-[300px]'>
+                <h2 className='text-xl text-center py-4'>Stay Organized, Stay Productive</h2>
+                <p className='text-sm text-center py-4 opacity-70'>
+                    Access your tasks! Log in now!
+                </p>
+                <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+                    <Input
+                        disabled={isPending}
+                        type='text'
+                        placeholder='username'
+                        {...register('username', { required: true })}
+                    />
+                    {errors.username && (
+                        <p className='text-xs text-red-500'>{errors.username.message}</p>
+                    )}
+                    <Input
+                        disabled={isPending}
+                        type='password'
+                        placeholder='password'
+                        {...register('password', { required: true })}
+                    />
+                    {errors.password && (
+                        <p className='text-xs text-red-500'>{errors.password.message}</p>
+                    )}
+                    <Button disabled={isPending} size='fullWidth' type='submit'>
+                        {isPending ? <LoadingSpinner size='xs' /> : <LogIn />}
+                    </Button>
+                </form>
+                <p className='text-sm text-center py-4 opacity-70'>
+                    Don’t have an account yet?{' '}
+                    <Link
+                        to='/signup'
+                        className='underline font-bold opacity-100 hover:text-blue-600'
+                    >
+                        Sign up here
+                    </Link>{' '}
+                    and start organizing your life!
+                </p>
+            </div>
+        </main>
+    );
+};
+
+export default LoginPage;
