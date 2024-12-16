bed8e771c5b7537fe9c4736a90fff2a5db55c001
Kris1027 <obarzanek.work@gmail.com>
2024-12-16 10:51:20 +0100

    build delete task functionality

--- client/src/components/task-item.tsx
+++ client/src/components/task-item.tsx
@@ -3,12 +3,29 @@ import { TaskProps } from '@/types/task-type';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
 import EditTask from './edit-task';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { toast } from '@/hooks/use-toast';
+import LoadingSpinner from './loading-spinner';
 
 export interface TaskItemProps {
     task: TaskProps;
 }
 
 const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
+    const queryClient = useQueryClient();
+    const { mutate: deleteTask, isPending } = useMutation({
+        mutationKey: ['tasks'],
+        mutationFn: async () => {
+            const res = await axios.delete(`/api/tasks/${task._id}`);
+            return res.data;
+        },
+        onSuccess: (data) => {
+            toast({ title: data.message, variant: 'positive' });
+            queryClient.invalidateQueries({ queryKey: ['tasks'] });
+        },
+    });
+
     const formatDate = (date: Date) =>
         new Date(date).toLocaleString('en-US', {
             year: 'numeric',
@@ -32,8 +49,8 @@ const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
                 </div>
                 <div className='space-x-4'>
                     <EditTask task={task} />
-                    <Button variant='destructive'>
-                        <Trash2 />
+                    <Button onClick={() => deleteTask()} variant='destructive'>
+                        {isPending ? <LoadingSpinner size='xs' /> : <Trash2 />}
                     </Button>
                 </div>
             </CardContent>
