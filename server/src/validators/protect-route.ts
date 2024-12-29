0731719c9c853e825705d57252abf1aa9c623b8a
kris1027 <krzys89@gmail.com>
2024-12-29 19:17:32 +0100

    refactor: enhance protectRoute to use IRequestWithCookies and improve error handling

--- server/src/validators/protect-route.ts
+++ server/src/validators/protect-route.ts
@@ -3,31 +3,40 @@ import User from '../models/user-model.js';
 import type { NextFunction, Request, Response } from 'express';
 import type { IErrorProps } from 'types/global.js';
 
-interface JwtPayloadWithUserIdProps extends jwt.JwtPayload {
+interface IRequestWithCookies extends Request {
+    cookies: {
+        jwt?: string;
+    };
+}
+
+interface IJwtPayloadWithUserIdProps extends jwt.JwtPayload {
     userId: string;
 }
 
-const protectRoute = async (req: Request, res: Response, next: NextFunction) => {
+const protectRoute = async (req: IRequestWithCookies, res: Response, next: NextFunction): Promise<void> => {
     try {
         const token = req.cookies.jwt;
         if (!token) {
             const error: IErrorProps = new Error('Unauthorized: No token provided');
             error.status = 401;
-            return next(error);
+            next(error);
+            return;
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayloadWithUserIdProps;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as IJwtPayloadWithUserIdProps;
         if (!decoded) {
             const error: IErrorProps = new Error('Unauthorized: Invalid token');
             error.status = 401;
-            return next(error);
+            next(error);
+            return;
         }
 
         const user = await User.findById(decoded.userId);
         if (!user) {
             const error: IErrorProps = new Error('Unauthorized: User not found');
             error.status = 401;
-            return next(error);
+            next(error);
+            return;
         }
 
         req.user = user;
