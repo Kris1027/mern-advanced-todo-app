04d4ac553ca51c3ffb6a153af34c231061aceb75
kris1027 <krzys89@gmail.com>
2024-12-29 17:55:12 +0100

    refactor: update logoutUser function to specify return type as void

--- server/src/controllers/auth-controller.ts
+++ server/src/controllers/auth-controller.ts
@@ -101,7 +101,7 @@ export const loginUser = async (
     }
 };
 
-export const logoutUser = async (req: Request, res: Response, next: NextFunction) => {
+export const logoutUser = (req: Request, res: Response, next: NextFunction): void => {
     try {
         res.cookie('jwt', '', { maxAge: 0 });
         res.status(200).json({ message: 'User logged out successfully' });
