8d60512b324e9b9a9d93cf08ac8f62a464f1a585
kris1027 <krzys89@gmail.com>
2025-01-08 17:41:09 +0100

    fix: update formatDate function to handle string input and change locale to en-GB

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,11 +5,15 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export const formatDate = (date: Date) =>
-    date.toLocaleString('en-US', {
+export const formatDate = (date: Date | string) => {
+    const d = new Date(date);
+    return d.toLocaleString('en-GB', {
         year: 'numeric',
-        month: 'short',
-        day: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
         hour: '2-digit',
         minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
     });
+};
